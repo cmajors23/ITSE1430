e829b5dabdb63f5161e246a6947ba2ab843b7155
clinton.majors <clinton.majors@my.tccd.edu>
2018-02-07 21:41:41 -0600

    272018

--- Classwork/Section2/Nile.Windows/MainForm.cs
+++ Classwork/Section2/Nile.Windows/MainForm.cs
@@ -16,5 +16,23 @@ namespace Nile.Windows
         {
             InitializeComponent();
         }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            var name = product.GetName();
+            //var product = new Product();
+            product.SetName("Product A");
+            // product.Description = "None";
+            var error = product.Validate();
+
+            var str = product.ToString();
+
+            var productB = new Product();
+            productB.SetName("Product B");
+            // productB.Description = product.Description;
+
+        }
     }
 }
